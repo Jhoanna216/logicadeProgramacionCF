63992fc93e343366e4f79f0aa987a5b999724b30
Jhoanna Nateras <82605058+Jhoanna216@users.noreply.github.com>
2024-09-03 00:58:49 -0600

    Second File - Introduction - main.py
    Module 1 - Module 3 - Class 1

---
+++ main.py
@@ -0,0 +1,16 @@
+# PEP-8
+
+# CamelCase
+
+
+class UserAdmin():
+
+    def __init__(self, username, password=''):  # Parámetros
+        self.username = username
+        self.password = password
+
+    def set_password(self):
+        pass
+
+# snake_case
+cody_user = UserAdmin('Cody')
